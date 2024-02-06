d5a6f289a5050cda2b7952503f39c226cc76868c
mufeng <mufeng@qq.com>
2024-02-06 22:21:43 +0800

    second Hello.java

--- test/src/com/mufeng/Hello.java
+++ test/src/com/mufeng/Hello.java
@@ -10,5 +10,6 @@ package com.mufeng;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
