f6ac5a138014210621bed9b2a63029c0c1191828
mufeng <mufeng@qq.com>
2024-02-06 22:15:09 +0800

    add Hello.java

---
+++ test/src/com/mufeng/Hello.java
@@ -0,0 +1,14 @@
+package com.mufeng;
+
+/**
+ * @author 沐枫
+ * @ClassName Hello
+ * @description: TODO
+ * @date 2024年02月06日
+ * @version: 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
